46c882adca2910e916111050156959abc223cb43
Jonasbha <jbh93hoved.annet@gmail.com>
2021-03-04 14:34:31 +0100

    more refactoring of config classes

--- src/main/java/webplate/page/component/Component.java
+++ src/main/java/webplate/page/component/Component.java
@@ -4,4 +4,20 @@ public abstract class Component {
 
     public Component() {}
 
+    public static abstract class ComponentConfig<E> {
+
+        protected E list;
+
+        protected boolean customizable = true;
+
+        abstract public void setList(E list);
+
+        public abstract boolean isCustomizable();
+
+        public void setCustomizable(boolean customizable) {
+            this.customizable = customizable;
+        }
+
+        public abstract void addToBottomOfPage();
+    }
 }
