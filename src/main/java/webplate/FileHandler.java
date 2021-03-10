6d3ebbcdaf8dc5e3583bce2658b72828fb6ae822
sondre5M1 <sondre.e.femmen@hiof.no>
2021-03-10 09:08:16 +0100

    adding create()

--- src/main/java/webplate/FileHandler.java
+++ src/main/java/webplate/FileHandler.java
@@ -1,6 +1,32 @@
 package webplate;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileHandler {
-    public void create(String page) {
+
+    public FileHandler() { }
+
+    public void create(String content, int index) {
+
+        try{
+            File file = new File(Webplate.pages.get(index).metadata.getTitle() + ".html");
+            if(file.createNewFile()){
+                System.out.println("File created: " + file.getName());
+            }
+            else {
+                System.out.println("File already exists");
+            }
+
+            FileWriter filskriver = new FileWriter(file.getName());
+            filskriver.write(content);
+            filskriver.close();
+            System.out.println("content written into file");
+
+        } catch (IOException e){
+            System.out.println("An error occured");
+            e.printStackTrace();
+        }
     }
 }
